7fb19b7a257b0224ed0acadcd75378f64f4c75be
maldikurniawan <akurniawan2102@gmail.com>
2022-08-15 14:27:21 +0700

    asdfg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="font-sans antialiased">
